04525848df03ba8b4afb1baca1bb3e281b09ab8b
Young Han <han20@umbc.edu>
2019-10-21 14:58:18 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,7 +26,7 @@ def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
-  
+  return assembled_matrix.sort()
   
 end
 
